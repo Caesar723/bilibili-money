9b38a8e1acd3a6e79275ab8dc72accdb14a9f859
caesar <76422688+Caesar723@users.noreply.github.com>
2021-02-19 09:01:56 +0800

    Update money.py

--- money.py
+++ money.py
@@ -8,7 +8,7 @@ caesar=0
 def comment(id,re):
     url = 'https://api.bilibili.com/x/web-interface/coin/add'
     header = {'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.1; rv:85.0) Gecko/20100101 Firefox/85.0",
-              'cookie': "_uuid=BDF32AB2-B3DC-EC56-963B-C9829A6C30DB06206infoc; buvid3=3709CE82-D37B-44E1-B327-A21981A1D0EB184990infoc; sid=8lps0bl6; fingerprint=cd2d3c15dfa8783a47570c3555c99405; buvid_fp=3709CE82-D37B-44E1-B327-A21981A1D0EB184990infoc; buvid_fp_plain=A92A84BD-5006-4DDB-B951-C78DA299B7EF184989infoc; DedeUserID=93254219; DedeUserID__ckMd5=147d5c8d721cbde8; SESSDATA=102be014%2C1628648354%2C12092*21; bili_jct=e631c26d38044b51b0a311883355eeb8; bp_video_offset_93254219=490725955173595632; bp_t_offset_93254219=491101610196193001; PVID=2; CURRENT_FNVAL=80; blackside_state=1; bsource=search_baidu; bfe_id=6f285c892d9d3c1f8f020adad8bed553",
+              'cookie': "",
               'referer':re
     }
     data = {
